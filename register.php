4e5c29ec46342705fed4eba44fa45c7b5203d9bc
Kim Truong-Trieu <truo0069@algonquinlive.com>
2025-11-23 12:04:39 -0500

    Changed SQL registration user, added temporary way to reset relevant database tables in register.php; minor text fixes

--- register.php
+++ register.php
@@ -3,24 +3,46 @@
 //    error_reporting(E_ALL);
 //    ini_set('display_errors', 'On');
 ?>
+    <form method="post"><input type="submit" name="submit" value="Reset Tables"></form>
     <form method="post">
         <label for="email">Email:</label><br>
         <input type="email" id="email" name="email" required><br>
         <label for="password">Password:</label><br>
         <input type="password" id="password" name="password" required><br>
-        <p><input type=submit name="submit" value="Register"></p>
+        <p><input type="submit" name="submit" value="Register"></p>
     </form>
 <?php
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $conn = new mysqli('localhost','auth','auth','HOBBYMART');
-        // Check for an existing email and sanitise inputs
-        $hashedPassword = password_hash($_POST['password'],PASSWORD_DEFAULT);
-        try {
-            mysqli_query($conn,"INSERT INTO USERS(email,password) VALUES ('{$_POST['email']}','{$hashedPassword}')");
-            echo "Registered.";
-        } catch (mysqli_sql_exception $e) {
-            echo "There was an issue with registration. Please try again or continue as a guest.";
+        switch ($_POST['submit']) {
+            case 'Register':
+                $conn = new mysqli('localhost','register','register','HOBBYMART');
+                // Check for an existing email and sanitise inputs 
+                $hashedPassword = password_hash($_POST['password'],PASSWORD_DEFAULT);
+                try {
+                    mysqli_query($conn,"INSERT INTO Users(email,password) VALUES ('{$_POST['email']}','{$hashedPassword}')");
+                    echo "Registered.";
+                } catch (mysqli_sql_exception $e) {
+                    echo "There was an issue with registration. Please try again or continue as a guest.";
+                }
+                mysqli_close($conn);
+                break;
+            case 'Reset Tables':
+                $conn = new mysqli('localhost','root', '', 'HOBBYMART');
+                $user = "register";
+                mysqli_query($conn, "DROP TABLE IF EXISTS Users");
+                mysqli_query($conn, "CREATE USER IF NOT EXISTS '" . $user .  "'@'localhost' identified by '" . $user . "'");
+                mysqli_query(
+                    $conn,
+                    "CREATE TABLE IF NOT EXISTS Users(
+                        userID INT AUTO_INCREMENT PRIMARY KEY,
+                        email VARCHAR(80) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        UNIQUE(email)
+                    )"
+                );
+                mysqli_query($conn, "GRANT SELECT, INSERT, UPDATE ON Users TO '" . $user . "'@'localhost'");
+                mysqli_close($conn);
+                break;
         }
-        mysqli_close($conn);
     }
 ?>
