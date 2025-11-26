700eebc878e1c6602c51e35cf28d0d06d1727a39
Kim Truong-Trieu <truo0069@algonquinlive.com>
2025-11-26 14:36:04 -0500

    logout.php to eliminate session

--- auth/logout.php
+++ auth/logout.php
@@ -1,1 +1,8 @@
-
+<?php
+    // Retrieve the existing session
+    session_start()
+    // Remove all session variables
+    session_unset();
+    // Destroy the session ie. log out
+    session_destroy();
+?>
